47ab08f4d6d91f70062b27d0c6e0d5096f8b2530
Thilan <thil4n@gmail.com>
2025-03-12 19:33:06 +0530

    Add Tailwind CSS configuration and refactor styles for improved design

--- view/tailwind.config.js
+++ view/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: [
+    "./index.html", // Add your HTML or JS/TS files here
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#0d1117",
+        card: "#161b22",
+        text: "#c9d1d9",
+        folder: "#8b949e",
+        file: "#8b949e",
+      },
+    },
   },
   plugins: [],
 };
